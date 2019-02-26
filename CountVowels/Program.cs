4dcf53d9565f1e4d0cd0c45b6b0418a79f2d9f1f
Alexander Anicete <alexander.anicete91@gmail.com>
2019-02-26 09:14:55 -0800

    Add project files.

---
+++ CountVowels/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getVowel("Hello my name is alex and i love people"));
+        }
+        public static int getVowel(string input)
+        {
+            int count = 0;
+            input.ToLower();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' )
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
